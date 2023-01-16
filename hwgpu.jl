25067113ec76d3e66f9a1b267e86a1ea3bc6e88c
Red <reed.polutta@outlook.com>
2023-01-15 21:15:06 -0500

    Updated gitignore

--- hwgpu.jl
+++
@@ -1,7 +0,0 @@
-include("train.jl")
-
-Model() = Chain(Dense(CityNum => 512, swish), LSTM(512 => 2048), Dense(2048 => 1024, swish), Dense(1024 => CityNum, σ))
-
-const tc = moveto(OnGPU, SmoothDescent(), load(TrainState, "trainstate", Model, ADAM()))
-
-train!(tc, 100)
